532651836f42b7d315f657103c97bcbfaae85c4a
Ethan Brydon <59847782+EthanBrydon@users.noreply.github.com>
2020-10-23 15:35:49 -0400

    Update dynamics.js

--- public/js/dynamics.js
+++ public/js/dynamics.js
@@ -10,6 +10,7 @@ class user {
         this.orders = [];
         this.watchlists = [];
         this.accounHistory = [];
+        this.filteredAccountHistory = [];
         this.alerts = [];
     }
 
@@ -23,6 +24,14 @@ class user {
         this.watchlists.pop(wlist); 
     }
 
+    watchlistsToString() {
+        var string = "Watchlists: "
+        for (w in this.watchlists) {
+            string += "\n\n" + w.name + ":\n" + w.toString();
+        }
+        return string + "\n\n";
+    }
+
     // alert methods
     createAlert(active, symbol, condition, overUnder) {
         var a = new alert(active, symbol, condition, overUnder);
@@ -33,30 +42,63 @@ class user {
         this.alerts.pop(alert);
     }
 
+    alertsToString() {
+        var string = "Alerts: \n";
+        for (a in this.alerts) {
+            string += "\n" + a.toString();
+        }
+        return string + "\n\n";
+    }
+
     // account history methods
-    logEvent(date, event, description, displaydate) {
-        var log = new historyLog(date, event, description, displaydate);
+    logEvent(date, event, description) {
+        var log = new historyLog(date, event, description);
         this.accounHistory.push(log);
+        this.filteredAccountHistory.push(log);
     }
 
     filterByEvent(event) {
+        var filtered = [];
         for (log in this.accounHistory) {
             //set all items of event to display
             // and others not too
-        } 
+            if (log.event === event) {
+                filtered.push(log);
+            } 
+        }
+        this.filteredAccounHistory = filtered; 
     }
 
     filterByDateRange(start, end) {
+        var filtered = [];
         for (log in this.accounHistory) {
             //set all items that are in date range to display
             // and others not too
-        } 
+            if (log.date >= start && log.date <= end) {
+                filtered.push(log);
+            } 
+        }
+        this.filteredAccounHistory = filtered;  
     }
 
     clearFilters() {
-        for (log in this.accounHistory) {
-            //set all items to display
+        this.filteredAccounHistory = this.accounHistory;
+    }
+
+    accounHistoryToString() {
+        var string = "Account History: \n"
+        for (l in this.accounHistory) {
+            string += "\n" + l.toString();
         }
+        return string + "\n\n";
+    }
+
+    filteredAccounHistoryToString() {
+        var string = "Filtered Account History: \n"
+        for (l in this.filteredAccounHistory) {
+            string += "\n" + l.toString();
+        }
+        return string + "\n\n";
     }
 
     // orders methods
@@ -99,9 +141,33 @@ class order {
         this.typeOrder = typeorder;
         this.priceEntered = price;
         this.numSharesOrdered = numShares;
-        this.expiry = expiry;
+        this.expireEndDay = expiry;
         // notification
+    }
+
+    cancelOrder() {
+        user.orders.pop(this);
+    }
+
+    toString() {
+        if (this.typeOrder) {
+            if (this.expireEndDay) {
+                return "Stock Ordered: " + this.symbol + " | Order Type: Buy | Number of Shares: " + this.numSharesOrdered + " | Price per Share: " + this.priceEntered + " | Expiry: Expires at the end of the day";              
+            }
+            else {
+                return "Stock Ordered: " + this.symbol + " | Order Type: Buy | Number of Shares: " + this.numSharesOrdered + " | Price per Share: " + this.priceEntered + " | Expiry: Expires upon cancellation";
+            }    
+        }
+        else {
+            if (this.expireEndDay) {
+                return "Stock Ordered: " + this.symbol + " | Order Type: Sell | Number of Shares: " + this.numSharesOrdered + " | Price per Share: " + this.priceEntered + " | Expiry: Expires at the end of the day";
+            }
+            else {
+                return "Stock Ordered: " + this.symbol + " | Order Type: Sell | Number of Shares: " + this.numSharesOrdered + " | Price per Share: " + this.priceEntered + " | Expiry: Expires upon cancellation";
+            }
+        }
     } 
+
 }
 
 class watchlist {
@@ -117,39 +183,101 @@ class watchlist {
     removeStockFromWatchlist(symbol) {
         this.stocks.pop(findStock(symbol));
     }
+
+    toString() {
+        var string = ""; 
+        for (s in this.stocks) {
+            string += "\n" + s.toString();
+        }
+        return string;
+    }
 }
 
 class alert  {
-    constructor(active, symbol, condition, overUnder) {
+    constructor(active, symbol, condition, increase) {
         this.active = active;
         this.symbol = symbol;
         this.condition = condition;
-        this.overUnder = overUnder;
+        this.increase = increase;
         // notification
     }
 
-    editAlert(condition, overUnder) {
+    editAlert(condition, increase) {
         this.condition = condition;
-        this.overUnder = overUnder;
+        this.increase = increase;
     }
 
     changeState() {
-        if (this.active === false ) {
-            this.active = true;
+        if (this.active) {
+            this.active = false;
         }
         else {
-            this.active = false;
+            this.active = true;
         }
     }
 
+    toString() {
+        if (this.increase) {
+            if (this.active) {
+                return this.symbol + " | Alert Condition: price increases by " + this.condition + "% | ACTIVE";
+            }
+            else {
+                return this.symbol + " | Alert Condition: price increases by " + this.condition + "% | INACTIVE";
+            }
+            
+        }
+        else {
+            if (this.active) {
+                return this.symbol + " | Alert Condition: price decreases by " + this.condition + "% | ACTIVE";
+            }
+            else {
+                return this.symbol + " | Alert Condition: price decreases by " + this.condition + "% | INACTIVE";
+            }
+        }  
+    }
 } 
 
 class historyLog {
-    constructor(date, event, description, displaydate) {
+    constructor(date, event, amount) {
         this.date = date;
-        eventType = event; //buy = 0, sell = 1, withdraw = 2, deposit = 3
-        this.description = description;
-        displayDate = displaydate;
+        this.eventType = event; //buy = 0, sell = 1, withdraw = 2, deposit = 3
+        this.amount = amount;
+        this.description = setDescriptionWD();
+    }
+
+    constructor(date, event, numShares, price, symbol) {
+        this.date = date;
+        this.symbol = symbol;
+        this.eventType = event; //buy = 0, sell = 1, withdraw = 2, deposit = 3
+        this.numShares = numShares;
+        this.price = price;
+        this.description = setDescriptionBS();
+    }
+
+    displayDate() {
+        return this.date.subString(0,2) + "-" + this.date.subString(2,4) + "-" + this.date.subString(4,8);
+    }
+
+    setDescriptionWD() {
+        if (Number(this.eventType) === 2) {
+            return "Withdrew " + this.amount + " from account.";
+        }
+        else {
+            return "Deposited " + this.amount + " into account.";
+        }
+    }
+
+    setDescriptionBS() {
+        if (Number(this.eventType) === 0) {
+            return "Bought " + this.numShares + " of " + this.symbol + " for " + Number(this.numShares*this.price) + " (" + this.price + " each)";
+        }
+        else {
+            return "Sold " + this.numShares + " of " + this.symbol + " for " + Number(this.numShares*this.price) + " (" + this.price + " each)";
+        }
+    }
+
+    toString() {
+        return this.displayDate() + "\n" +  this.description;  
     }
 }
 
@@ -209,6 +337,8 @@ function findStock(symbol) {
         }
     }
 }
+
+
 
 
 
